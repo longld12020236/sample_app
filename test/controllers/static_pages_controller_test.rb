1469308b0030764b07e5c28364b42b431de317d7
Ly Duc Long <longlyduc@gmail.com>
2016-10-31 14:08:07 +0000

    Them trang contact

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,18 +8,23 @@ class StaticPagesControllerTest < ActionController::TestCase
 		get :home
     	assert_response :success
     	assert_select "title", "Home | #{@base_title}"
-  	end
+  end
 
-  	test "should get help" do
+  test "should get help" do
     	get :help
     	assert_response :success
     	assert_select "title", "Help | #{@base_title}"
-  	end
+  end
 
-  	test "should get about" do
+  test "should get about" do
   		get :about
   		assert_response :success
   		assert_select "title", "About | #{@base_title}"
-  	end
+  end
+
+  test "should get contact" do
+      get :contact
+      assert_response :success
+      assert_select "title", "Contact | #{@base_title}"
 
 end
